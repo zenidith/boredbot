b177821261dd6fd8ce53cff56ad61d282d440ffa
K Sheenan <kaynesheenan+git@gmail.com>
2021-07-26 23:57:03 +0900

    more styling

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ document.getElementById("get-activity").addEventListener("click", function() {
     fetch("https://apis.scrimba.com/bored/api/activity")
     .then(response => response.json())
     .then(data => {
-      document.getElementById("activity").textContent = data.activity    
+      document.getElementById("activity").textContent = data.activity
+      document.getElementById("title").textContent = "🔫 here's your random ass activity! 🎯"    
+      document.body.classList.add("bot-activity")
       })
 })
